d6ed5a8011513e61f8406c5ea8229b96bd1c423f
Satyendra Sharma <satyendra.sharma@genpact.com>
2015-08-19 11:42:44 +0530

    LoginAction implemented with initial functionalities

---
+++ LearningWeb/src/com/satya/learn/LoginAction.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package com.satya.learn;
+
+/**
+ * @author 703162557
+ *
+ */
+public class LoginAction {
+
+	/**
+	 * 
+	 */
+	public LoginAction() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
